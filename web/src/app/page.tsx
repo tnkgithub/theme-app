42ef468352ac0f39d2e83cba3b973950c717a61e
tnkgithub <tnk.github@gmail.com>
2024-09-12 14:29:32 +0900

    imagespageをrepresentationPageに変更&遷移時にアニメーションを追加

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <div className='container mx-auto flex h-full flex-wrap items-center justify-center p-10 lg:justify-between'>
-        <Link href='/imagespage'>
+        <Link href='/representationPage'>
           <HomeCard
             title='画像一覧'
             description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic amet odit facere quia modi laborum enim velit assumenda aspernatur aliquam est perferendis cupiditate, eius vel officia eligendi ex nisi esse.'
---
+++ web/src/app/representationPage/page.tsx
@@ -0,0 +1,410 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function ImagesPage() {
+  return (
+    <div className='no-scrollbar'>
+      <div className='m-2 grid grid-cols-26 gap-1'>
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+
+        <div className='bg-blue-400'>
+          <Link
+            href='https://archives.c.fun.ac.jp/posters/po000001/0001'
+            rel='noreferrer noopener'
+            target='_blank'
+          >
+            <Image
+              src='/posters/trimmedPoster/po000001.jpg'
+              alt='1'
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+        <div className='bg-blue-400'>7th col</div>
+        <div className='bg-blue-400'>8th col</div>
+        <div className='bg-blue-400'>9th col</div>
+        <div className='bg-blue-900'>10th col</div>
+        <div className='bg-blue-400'>1st col</div>
+        <div className='bg-blue-400'>2nd col</div>
+        <div className='bg-blue-400'>3rd col</div>
+        <div className='bg-blue-400'>4th col</div>
+        <div className='bg-blue-400'>5th col</div>
+        <div className='bg-blue-400'>6th col</div>
+      </div>
+    </div>
+  );
+}
