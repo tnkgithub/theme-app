9fd65b3d88d1554e50f69073dc8c4d4e6e90f0fe
tnkgithub <tnk.github@gmail.com>
2024-09-30 16:29:31 +0900

    added data to database

---
+++ web/src/lib/prisma/importData.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const results = [];
+
+  fs.createReadStream('public/tmp/data/posterData.csv') // パスを修正
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', async () => {
+      for (const item of results) {
+        // console.log(item);
+        await prisma.poster.create({
+          data: {
+            id: item.id,
+            title: item.title,
+            explanation: item.explanation,
+            somCoordinate: parseInt(item.somCoordinate, 10), // 数値に変換
+            representationsCoordinate: !isNaN(
+              parseInt(item.representationsCoordinate, 10)
+            )
+              ? parseInt(item.representationsCoordinate, 10)
+              : null,
+          },
+        });
+      }
+      console.log('Data imported successfully!');
+      await prisma.$disconnect();
+    });
+}
+
+main().catch((e) => {
+  console.error(e);
+  prisma.$disconnect();
+});
