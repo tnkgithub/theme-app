3b871781e62d188761e6ede5270fb2d06d77d9b7
tnkgithub <tnk.github@gmail.com>
2024-12-17 18:11:18 +0900

    a

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -7,31 +7,29 @@ export default function Home() {
   return (
     <MotionWrapper>
       <main>
-        <Suspense fallback={<div>Loading...</div>}>
-          <div className='container mx-auto flex h-full flex-wrap items-center justify-center gap-x-14 p-2'>
-            <Link href='/representation'>
-              <Card
-                title='画像一覧'
-                // description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic amet odit facere quia modi laborum enim velit assumenda aspernatur aliquam est perferendis cupiditate, eius vel officia eligendi ex nisi esse.'
-                type='imagesList'
-              />
-            </Link>
-            <Link href='/titles'>
-              <Card
-                title='タイトル一覧'
-                // description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic amet odit facere quia modi laborum enim velit assumenda aspernatur aliquam est perferendis cupiditate, eius vel officia eligendi ex nisi esse.'
-                type='titleList'
-              />
-            </Link>
-            <Link href='/objectData'>
-              <Card
-                title='物体名一覧'
-                // description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic amet odit facere quia modi laborum enim velit assumenda aspernatur aliquam est perferendis cupiditate, eius vel officia eligendi ex nisi esse.'
-                type='objectList'
-              />
-            </Link>
-          </div>
-        </Suspense>
+        <div className='container mx-auto flex h-full flex-wrap items-center justify-center gap-x-14 p-2'>
+          <Link href='/representation'>
+            <Card
+              title='画像一覧'
+              // description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic amet odit facere quia modi laborum enim velit assumenda aspernatur aliquam est perferendis cupiditate, eius vel officia eligendi ex nisi esse.'
+              type='imagesList'
+            />
+          </Link>
+          <Link href='/titles'>
+            <Card
+              title='タイトル一覧'
+              // description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic amet odit facere quia modi laborum enim velit assumenda aspernatur aliquam est perferendis cupiditate, eius vel officia eligendi ex nisi esse.'
+              type='titleList'
+            />
+          </Link>
+          <Link href='/objectData'>
+            <Card
+              title='物体名一覧'
+              // description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic amet odit facere quia modi laborum enim velit assumenda aspernatur aliquam est perferendis cupiditate, eius vel officia eligendi ex nisi esse.'
+              type='objectList'
+            />
+          </Link>
+        </div>
       </main>
     </MotionWrapper>
   );
